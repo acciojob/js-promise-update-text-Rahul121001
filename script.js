ffb75c0c47187ea6f209790089ae56102b0455ae
Rahul121001 <139845999+Rahul121001@users.noreply.github.com>
2023-10-23 20:21:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function delayedHelloWorld() {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve("Hello, world!");
+                }, 1000);
+            });
+        }
+
+        // Wait for the promise to resolve and then update the element
+        delayedHelloWorld()
+            .then((message) => {
+                document.getElementById("output").textContent = message;
+            })
+            .catch((error) => {
+                console.error(error);
+            });
